eb7319353be08c9c55243ea2c0a91e9aef427bf7
JeetDas5 <jeet15083011@gmail.com>
2025-10-15 23:29:54 +0530

    feat: create sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import InfoBar from "@/components/infobar";
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 type Props = { children: React.ReactNode };
@@ -5,7 +7,11 @@ type Props = { children: React.ReactNode };
 const Layout = ({ children }: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
-      <div className="w-full">{children}</div>
+      <Sidebar />
+      <div className="w-full">
+        <InfoBar />
+        {children}
+      </div>
     </div>
   );
 };
